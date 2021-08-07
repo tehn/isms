43d993de2919c85118b7c53b73e56352b172bd0a
brian crabtree <tehn@monome.org>
2021-08-07 16:47:59 -0400

    example

---
+++ example.lua
@@ -0,0 +1,16 @@
+--- script
+print("hello there")
+a = 8
+b = 3
+print(a+b)
+
+for i=1,200 do
+  screen.pixel(10+i,20,0xFFFFFF-i);
+end
+
+screen.redraw()
+
+key = function(x)
+  screen.pixel(math.random(200),math.random(200),0xFFFFFF);
+  screen.redraw()
+end
