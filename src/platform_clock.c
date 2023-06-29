16b3f908a337fa96e8cb5fae1a2f1973e9db137c
Artem Popov <artfwo@gmail.com>
2023-06-29 15:27:48 +0200

    null check in emulated_clock_nanosleep

--- src/platform_clock.c
+++ src/platform_clock.c
@@ -10,19 +10,27 @@ static int emulated_clock_nanosleep(clockid_t clock_id, int flags,
                                     const struct timespec *rqtp,
                                     struct timespec *rmtp) {
   struct timespec timespec_init;
+
   clock_gettime(clock_id, &timespec_init);
+
   switch (clock_id) {
   case CLOCK_REALTIME:
   case CLOCK_MONOTONIC: {
     uint64_t nanos = rqtp->tv_sec * IV_1E9 + rqtp->tv_nsec;
     int success;
+
     if ((flags & TIMER_ABSTIME)) {
       uint64_t back = timespec_init.tv_sec * IV_1E9 + timespec_init.tv_nsec;
       nanos = nanos > back ? nanos - back : 0;
     }
+
     success = mach_wait_until(mach_absolute_time() + nanos) == KERN_SUCCESS;
-    rmtp->tv_sec = 0;
-    rmtp->tv_nsec = 0;
+
+    if (rmtp != NULL) {
+      rmtp->tv_sec = 0;
+      rmtp->tv_nsec = 0;
+    }
+
     return success;
   }
   default:
